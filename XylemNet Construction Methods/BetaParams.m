9640ee96a02436542a7df4fae0e975d4b3593bc8
mradassaad <mradassaad2@gmail.com>
2018-06-01 17:21:21 -0400

    Delete BetaParams.m

--- XylemNet Construction Methods/BetaParams.m
+++
@@ -1,8 +0,0 @@
-function [A,B] = BetaParams(mu,sig)
-
-t = mu/(1-mu);
-f = @(B)sig^2 - t*B^2/(B^2*(1-t)^2*(B*(1+t)+1));
-B = fzero(f,[0.1 200]); % Solve for shape parameter B
-A = t*B; % Substitue to find shape parameter A
-
-end
