fce465c821a645bea8ddc32abe43b88d34772866
Assaad Mrad <mradassaad2@gmail.com>
2018-06-05 19:23:02 -0400

    Make VCGen more readable

--- VCGen.m
+++ VCGen.m
@@ -1,20 +1,17 @@
-% net = sim;
-% %jt: number of VCs to be generated
-% jt = 5;
-% stepsize = 0.4; %MPa
-% %itmax: max number of bubble pressure increases
-% itmax = 16; 
-% %inEmb: one initial embolism in every cluster
-% inEmb = 1; 
-% %AvgBool:
-% %True - output averaged VC
-% %False - output each generation
-% AvgBool = false;
+net = sim;
+%jt: number of VCs to be generated
+jt = 5;
+stepSize = 0.4; %MPa
+%itmax: max number of bubble pressure increases
+itmax = 16; 
+%inEmb: one initial embolism in every cluster
+inEmb = 1; 
+%AvgBool:
+%True - output averaged VC
+%False - output each generation
+AvgBool = false;
 % 
-% % profile on
-% [PLC,RWD,Pressures,Kmax,Kini,voltot,embNb,ncNb,Embolized,Penetrated] = ...
-%     cavitation_process(net,jt,stepsize,itmax,inEmb, AvgBool);
-% % profile viewer
 
 [mVC.PLC,mVC.PV,mVC.Pressures,mVC.Kmax,mVC.Kini,mVC.voltot,mVC.embNb,...
-    mVC.ncNb,Emb,Pen] = cavitation_process(sim,1,0.1,50,1,false);
+    mVC.ncNb,Emb,Pen] = cavitation_process(sim,jt,stepSize,itmax,inEmb,...
+    AvgBool);
